98caf7313e99e765f05546cc3fc055658e37f0e4
yacinbenhamida <yacinbenhamida@hotmail.fr>
2017-03-20 18:13:50 +0100

    ouvoouwvwewvwe onyetnwevwewve umbegumbwem ossas ouvoouwvwewvwe onyetnwevwewve umbegumbwem ossas ouvoouwvwewvwe onyetnwevwewve umbegumbwem ossas

--- app/src/main/java/application/facturia/Facturia.java
+++ app/src/main/java/application/facturia/Facturia.java
@@ -2,6 +2,7 @@ package application.facturia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Facturia extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class Facturia extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facturia);
+    }
+
+    public void ctnConnectClick(View view) {
+
+    }
+
+    public void btnInscritClick(View view) {
     }
 }
