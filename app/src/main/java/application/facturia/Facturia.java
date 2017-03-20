663bb7dcfb75ec59606ad29745f7771b62666b43
yacinbenhamida <yacinbenhamida@hotmail.fr>
2017-03-20 17:09:09 +0100

    Initial commit

---
+++ app/src/main/java/application/facturia/Facturia.java
@@ -0,0 +1,13 @@
+package application.facturia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Facturia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facturia);
+    }
+}
